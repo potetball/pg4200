511d0b30c706739b21dee1daf041536f4e1802b7
Erik Alvarez Tyldum <erik@garnesdata.no>
2018-10-21 22:09:35 +0200

    Update

---
+++ exercises/src/main/java/org/pg4200/ex01/MyArrayUtils.java
@@ -0,0 +1,34 @@
+package org.pg4200.ex01;
+
+public class MyArrayUtils implements ArrayUtils {
+
+    /**
+     * @param array
+     * @return the minimum value in the array
+     * @throws IllegalArgumentException if input is null or empty
+     */
+    @Override
+    public int min(int[] array) throws IllegalArgumentException {
+        return 0;
+    }
+
+    /**
+     * @param array
+     * @return the maximum value in the array
+     * @throws IllegalArgumentException if input is null or empty
+     */
+    @Override
+    public int max(int[] array) throws IllegalArgumentException {
+        return 0;
+    }
+
+    /**
+     * @param array
+     * @return the arithmetic average of the values in the array
+     * @throws IllegalArgumentException if input is null or empty
+     */
+    @Override
+    public double mean(int[] array) throws IllegalArgumentException {
+        return 0;
+    }
+}
