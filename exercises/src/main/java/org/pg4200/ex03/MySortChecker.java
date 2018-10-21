f578d91c9eb0306bbd93145073ac4c9db6fa9348
Erik Alvarez Tyldum <erik@garnesdata.no>
2018-10-21 22:48:11 +0200

    Ex06

--- exercises/src/main/java/org/pg4200/ex03/MySortChecker.java
+++ exercises/src/main/java/org/pg4200/ex03/MySortChecker.java
@@ -14,13 +14,53 @@ public class MySortChecker implements SortChecker {
      */
     @Override
     public <T extends Comparable<T>> boolean isSortedCopy(T[] original, T[] sorted) {
-        if (original == null || sorted == null) return false;
-        //first we need to sort the original, then we can compare it to see if its sorted
-        for (int i = 0; i < sorted.length; i++) {
-            if (original[i] != sorted[i]) {
+
+        if(original == null && sorted == null){
+            return true;
+        }
+
+        if(original == null || sorted == null){
+            return false;
+        }
+
+        if(original.length != sorted.length){
+            return false;
+        }
+
+        //null elements?
+        for(int i=0; i<sorted.length; i++) {
+            if(sorted[i]==null || original[i]==null){
                 return false;
             }
         }
-        return false;
+
+        //is it sorted?
+        for(int i=0; i<sorted.length-1; i++){
+            if(sorted[i].compareTo(sorted[i+1]) > 0){
+                return false;
+            }
+        }
+
+        //is it a permutation?
+        for(int i=0; i<sorted.length; i++) {
+
+            T value = sorted[i];
+            int originalCount = 0;
+            int sortedCount = 0;
+
+            for(int j=0; j<sorted.length; j++) {
+                if(sorted[j].equals(value)){
+                    sortedCount++;
+                }
+                if(original[j].equals(value)){
+                    originalCount++;
+                }
+            }
+            if(originalCount != sortedCount){
+                return false;
+            }
+        }
+
+        return true;
     }
 }
