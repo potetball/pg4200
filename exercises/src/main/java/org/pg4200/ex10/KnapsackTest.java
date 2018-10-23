f8c2691050229bd7221ffcbd39b83492f667bb09
Erik Alvarez Tyldum <erik@garnesdata.no>
2018-10-23 10:02:30 +0200

    ex10

---
+++ exercises/src/main/java/org/pg4200/ex10/KnapsackTest.java
@@ -0,0 +1,11 @@
+package org.pg4200.ex10;
+import org.pg4200.les10.knapsack.BruteForceForKnapsack;
+import org.pg4200.les10.knapsack.KnapsackProblem;
+
+
+
+public class KnapsackTest {
+    private void atest(){
+
+    }
+}
